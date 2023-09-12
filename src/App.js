b8c3eb4e5ec3f67819032c9c5ac76a3a3ab17cb1
Alex H <alex.holder@bigfootds.com>
2023-09-12 20:48:33 +1000

    feat: fruit list in D.R.Y JSX component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// Import custom JSX element:
+import FruitList from './FruitList';
+
+// some comment here 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+      <ul>
+        {/* Array of li tags with fruit names as contents */}
+        {/* Use custom JSX element: */}
+        <FruitList /> 
+      </ul>
     </div>
   );
 }
---
+++ src/FruitList.jsx
@@ -0,0 +1,37 @@
+// Array of fruit names:
+let someFruit = [
+	"mangoes",
+	"peaches",
+	"apples",
+	"blueberries",
+	"aubergine",
+	"secret banana",
+	"papayas",
+	"banana",
+	"durian",
+	"tomatoes",
+	"rambutan",
+	"mulberries", 
+	"kiwis",
+	"jackfruit",
+	"watermelon",
+	"zach fruit",
+	"grape",
+	"coconut"
+]
+  
+// Process someFruit and build an array of li tags 
+let fruitList = someFruit.map((fruit => {
+return <li>{fruit}</li>
+}));
+
+// Functions MUST return JSX, if you wanna use it as HTML
+// a.k.a in place of inline JSX:
+export default function FruitList(){
+	return fruitList;
+}
+
+// module.exports = FruitList;
+// module.exports = {
+// 	FruitList
+// }
